9cb9b75d3c9081d9e53d412cf14a01f1d1d7002c
Jeremy Swartwood <jeremy.swartwood@gmai.com>
2018-11-26 22:29:53 -0900

    Customize SilverBlock so it has metal sounds and characteristics

--- src/main/java/com/twojeremys/vampiric/init/ModBlocks.java
+++ src/main/java/com/twojeremys/vampiric/init/ModBlocks.java
@@ -1,6 +1,7 @@
 package com.twojeremys.vampiric.init;
 
 import com.twojeremys.vampiric.blocks.BlockBase;
+import com.twojeremys.vampiric.blocks.SilverBlock;
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
 
@@ -10,5 +11,5 @@ import java.util.List;
 public class ModBlocks {
     public static final List<Block> BLOCKS = new ArrayList<Block>();
 
-    public static final Block SILVER_BLOCK = new BlockBase("silver_block", Material.IRON);
+    public static final Block SILVER_BLOCK = new SilverBlock("silver_block", Material.IRON);
 }
