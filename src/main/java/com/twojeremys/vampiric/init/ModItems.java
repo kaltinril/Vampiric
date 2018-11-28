dbe65cc2360076bd994656adb1f9bfdec8550d3d
Jeremy Swartwood <jeremy.swartwood@gmai.com>
2018-11-27 23:03:07 -0900

    Add garlic food item

--- src/main/java/com/twojeremys/vampiric/init/ModItems.java
+++ src/main/java/com/twojeremys/vampiric/init/ModItems.java
@@ -1,6 +1,7 @@
 package com.twojeremys.vampiric.init;
 
 import com.twojeremys.vampiric.items.ItemBase;
+import com.twojeremys.vampiric.items.food.FoodBase;
 import net.minecraft.item.Item;
 
 import java.util.ArrayList;
@@ -10,5 +11,10 @@ public class ModItems {
 
     public static final List<Item> ITEMS = new ArrayList<Item>();
 
+    // Items
     public static final Item SILVER_INGOT = new ItemBase("silver_ingot");
+
+    // Food
+    // https://minecraft.gamepedia.com/Hunger
+    public static final Item GARLIC = new FoodBase("garlic", 1, 0.6f, false);
 }
