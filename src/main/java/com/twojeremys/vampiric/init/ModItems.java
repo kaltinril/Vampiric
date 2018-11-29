c6db4c461b49219643698424bcd60876557de406
Jeremy Swartwood <jeremy.swartwood@gmai.com>
2018-11-28 23:57:33 -0900

    Add a basic Oak Stake.

--- src/main/java/com/twojeremys/vampiric/init/ModItems.java
+++ src/main/java/com/twojeremys/vampiric/init/ModItems.java
@@ -2,7 +2,12 @@ package com.twojeremys.vampiric.init;
 
 import com.twojeremys.vampiric.items.ItemBase;
 import com.twojeremys.vampiric.items.food.GarlicPlant;
+import com.twojeremys.vampiric.items.tools.ToolStake;
 import net.minecraft.item.Item;
+import net.minecraft.item.Item.ToolMaterial;
+import net.minecraft.item.ItemSword;
+import net.minecraft.item.ItemTool;
+import net.minecraftforge.common.util.EnumHelper;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -11,6 +16,9 @@ public class ModItems {
 
     public static final List<Item> ITEMS = new ArrayList<Item>();
 
+    // Materials
+    public static final ToolMaterial MATERIAL_STAKE = EnumHelper.addToolMaterial("material_stake", 0, 59, 12.0f, 6.0f, 0);
+
     // Items
     public static final Item SILVER_INGOT = new ItemBase("silver_ingot");
     public static final Item GARLIC_PASTE = new ItemBase("garlic_paste");
@@ -18,4 +26,7 @@ public class ModItems {
     // Food
     // https://minecraft.gamepedia.com/Hunger
     public static final Item GARLIC = new GarlicPlant("garlic", 1, 0.6f, false);
+
+    // Tools
+    public static final ItemSword OAK_STAKE = new ToolStake("oak_stake", MATERIAL_STAKE);
 }
---
+++ src/main/java/com/twojeremys/vampiric/items/tools/ToolStake.java
@@ -0,0 +1,49 @@
+package com.twojeremys.vampiric.items.tools;
+
+import com.twojeremys.vampiric.Main;
+import com.twojeremys.vampiric.init.ModItems;
+import com.twojeremys.vampiric.util.IHasModel;
+import net.minecraft.client.util.ITooltipFlag;
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.ItemSword;
+import net.minecraft.world.World;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
+
+import javax.annotation.Nullable;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ToolStake extends ItemSword implements IHasModel {
+
+    public ToolStake(String name, ToolMaterial material){
+        super(material);
+        setUnlocalizedName(name);
+        setRegistryName(name);
+        setCreativeTab(CreativeTabs.MATERIALS);
+
+
+        // Add this item to the list of items in minecraft
+        ModItems.ITEMS.add(this);
+    }
+
+    @Override
+    public void registerModels() {
+        Main.proxy.registerItemRenderer(this, 0, "Inventory");
+    }
+
+    // Oddly, this is how you add text to the tooltip???
+    // https://minecraft.gamepedia.com/Formatting_codes   (Colors of text)
+    @Override
+    public void addInformation(ItemStack stack, @Nullable World worldIn, List<String> tooltip, ITooltipFlag flagIn) {
+        tooltip.add("\u00A74Stakes are useful against vampires!");
+        tooltip.add("\u00A74Pierce their heart to kill them!");
+        super.addInformation(stack, worldIn, tooltip, flagIn);
+    }
+
+    //TODO: Make sure that we only work when attacking a bat.
+    
+}
