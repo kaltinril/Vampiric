81864206fec826265c786b977aa887e496f1b140
Jeremy Swartwood <jeremy.swartwood@gmai.com>
2020-01-20 19:59:35 -0900

    Add silver_block back in

---
+++ src/main/java/com/kaltinril/vampiric/lists/BlockList.java
@@ -0,0 +1,8 @@
+package com.kaltinril.vampiric.lists;
+
+import net.minecraft.block.Block;
+
+public class BlockList {
+    public static Block silver_block;
+
+}
---
+++ src/main/java/com/kaltinril/vampiric/lists/ItemList.java
@@ -0,0 +1,8 @@
+package com.kaltinril.vampiric.lists;
+
+import net.minecraft.item.Item;
+
+public class ItemList {
+    public static Item silver_block;
+    public static Item silver_ingot;
+}
