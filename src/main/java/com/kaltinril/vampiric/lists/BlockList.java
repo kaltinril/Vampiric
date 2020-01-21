d43ea4b8c60a74ca6e5261a214cfc9142d663115
Jeremy Swartwood <jeremy.swartwood@gmai.com>
2020-01-20 21:57:08 -0900

    Change to not use static without the ObjectHolder and FINAL modifiers

--- src/main/java/com/kaltinril/vampiric/lists/BlockList.java
+++ src/main/java/com/kaltinril/vampiric/lists/BlockList.java
@@ -1,9 +1,13 @@
 package com.kaltinril.vampiric.lists;
 
+import com.kaltinril.vampiric.VampiricMod;
 import net.minecraft.block.Block;
+import net.minecraftforge.registries.ObjectHolder;
 
+@ObjectHolder(VampiricMod.modid)
 public class BlockList {
-    public static Block silver_block;
-    public static Block silver_ore;
+    // https://gist.github.com/Cadiboo/fbea89dc95ebbdc58d118f5350b7ba93#recommended-forge-conventions
+    public static final Block silver_block = null;
+    public static final Block silver_ore = null;
 
 }
--- src/main/java/com/kaltinril/vampiric/lists/ItemList.java
+++ src/main/java/com/kaltinril/vampiric/lists/ItemList.java
@@ -1,9 +1,13 @@
 package com.kaltinril.vampiric.lists;
 
+import com.kaltinril.vampiric.VampiricMod;
 import net.minecraft.item.Item;
+import net.minecraftforge.registries.ObjectHolder;
 
+@ObjectHolder(VampiricMod.modid)
 public class ItemList {
-    public static Item silver_block;
-    public static Item silver_ore;
-    public static Item silver_ingot;
+    // https://gist.github.com/Cadiboo/fbea89dc95ebbdc58d118f5350b7ba93#recommended-forge-conventions
+    public static final Item silver_block = null;
+    public static final Item silver_ore = null;
+    public static final Item silver_ingot = null;
 }
